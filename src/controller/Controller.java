92a016c5e53bc5a7a906559d272eea52d63607c3
nicholashaynes1 <nicholashaynes1@gmail.com>
2019-07-18 19:06:41 -0600

    I think this is in a working state now.

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -9,22 +9,34 @@ public class Controller
 	
 	public void start()
 	{
-		System.out.println(camelCase("a b  c"));
+		System.out.println(camelCase("a b  c hello world"));
 	}
 	
 	
 	public String camelCase(String str)
 	{
 		String returnMe = "";
-		
 		String[] wordArray = str.split("");
+		int spaceIndex = 0;
+		
 		
 		for(int i = 0; i < wordArray.length; i++)
 		{
-			
-			if(wordArray[i].contains(" ") == false)
+			if(i == 0)
 			{
-				System.out.println(wordArray[i] + " was the letter picked up");
+				wordArray[i] = wordArray[i].toUpperCase();
+			}
+			if(wordArray[i].contains(" "))
+			{
+				spaceIndex = i;
+			}
+			else
+			{
+				if(i == spaceIndex + 1)
+				{
+					wordArray[i] = wordArray[i].toUpperCase();
+				}
+				returnMe = returnMe.concat(wordArray[i]);
 			}
 		}
 		
