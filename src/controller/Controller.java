5093c5cdb4d96bfeb8ec9d2dc003d75c2367661c
nicholashaynes1 <nicholashaynes1@gmail.com>
2019-07-22 16:15:46 -0600

    Added the top voted solution as well.

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -10,6 +10,7 @@ public class Controller
 	public void start()
 	{
 		System.out.println(camelCase("Hello case method"));
+		System.out.println(neatSolution("Hello case method"));
 	}
 	
 	
@@ -44,5 +45,23 @@ public class Controller
 		
 		return returnMe;
 		
+	}
+	
+	
+	private String neatSolution(String str)
+	{
+		// your code here
+        String[] strings = str.split(" ");
+        StringBuilder stringBuilder=new StringBuilder();
+        for (String string : strings) 
+		{
+        	if (string.length()>0)
+        	{
+            stringBuilder.append(string.replaceFirst(string.substring(0, 1), string.substring(0, 1).toUpperCase()));
+		    }
+
+		 }
+        return new String(stringBuilder);
+
 	}
 }
